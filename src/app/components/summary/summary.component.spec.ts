5d1a22513a35bc5b863d3787f016fbacf947dc75
Julian Holfeld <julian@uni-kassel.de>
2022-03-14 11:10:32 +0100

    refactor: format summary component

--- src/app/components/summary/summary.component.spec.ts
+++ src/app/components/summary/summary.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SummaryComponent } from './summary.component';
+import {SummaryComponent} from './summary.component';
 
 describe('SummaryComponent', () => {
   let component: SummaryComponent;
@@ -8,9 +8,9 @@ describe('SummaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SummaryComponent ]
+      declarations: [SummaryComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
